a026ee8d1c60538a9039b35329f9921d9abd9bc9
DonGamero <gamerofuentegrande@gmail.com>
2024-12-16 13:09:40 +0100

    Add files via upload

---
+++ Practica3_2_Componente/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+
+
+			Scene scene = new Scene(root);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
